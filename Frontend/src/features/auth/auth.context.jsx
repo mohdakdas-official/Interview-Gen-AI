04527332fb1b81f8d701875421a5ff12323f5ee3
mohdaqdas-official <227436609+mohdaqdas-official@users.noreply.github.com>
2026-06-03 17:02:12 +0530

    Small Bug Fixes

--- Frontend/src/features/auth/auth.context.jsx
+++ Frontend/src/features/auth/auth.context.jsx
@@ -7,8 +7,6 @@ export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
 
-  
-
   return (
     <AuthContext.Provider value={{ user, setUser, loading, setLoading }}>
       {children}
